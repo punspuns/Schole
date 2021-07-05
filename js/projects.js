bec670445a10ab64bee15e0de622a741cda0f1a6
punspuns <74218652+punspuns@users.noreply.github.com>
2021-07-06 00:23:21 +0900

    nhieu

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
     $('#team-pr').progress();
     $('#inline_calendar').calendar();
+    equalheight('.ui.card div.image');
+    $(window).on('resize', function() {
+        equalheight('.ui.card div.image')
+    });
  });
+
+ equalheight = function(container){
+    var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+
+    $(container).each(function() {
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+}
