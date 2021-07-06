283db9108b9747ed3fa69cf74fc9dbc7aa65b7e8
punspuns <74218652+punspuns@users.noreply.github.com>
2021-07-06 15:20:55 +0900

    stuff

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,26 @@
-$(document).ready(function() {
-    $('#team-pr').progress();
-    $('#inline_calendar').calendar();
-    $('.ui.checkbox').checkbox('set checked');
- });
-
- $('button').click(
-    $('.ui.modal').modal('show')
- ) 
-
+$(document).ready(function () {
+   $('#team-pr').progress();
+   $('#inline_calendar').calendar({
+      initialDate: new Date('2020-12-1'),
+      eventDates: [{
+         date: new Date('2020-12-12'),
+         message: 'Show me in light purple',
+         class: 'inverted purple'
+      }, {
+         date: new Date('2020-12-25'),
+         message: 'Show me in green',
+         class: 'green'
+      }]
+   });
+   $('#task-cal').calendar({
+      type: 'date'
+    });
+   $('.ui.checkbox').checkbox('set checked');
+   $('#schedule').click(function () {
+      $('#checklist').modal('show')
+   })
+   $('#task').click(function () {
+      $('#add-task').modal('show')
+   })
+   $('.tabular.menu .item').tab();
+});
