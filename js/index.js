47a7a0d8dca04a6168d482dc7de4039e90a76b1d
punspuns <74218652+punspuns@users.noreply.github.com>
2021-07-08 01:55:26 +0900

    xong

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    $('#inline_calendar').calendar();
+    $('#inline_calendar').calendar({
+        initialDate: new Date('2020-12-1'),
+        eventDates: [{
+           date: new Date('2020-12-24'),
+           message: 'Show me in light purple',
+           class: 'inverted purple'
+        }, {
+           date: new Date('2020-12-27'),
+           message: 'Show me in green',
+           class: 'green'
+        }]
+     });
     $('#example1').progress();
     $('.ui.dropdown').dropdown();
  });
